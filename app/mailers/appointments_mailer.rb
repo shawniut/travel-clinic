4ae63f7ea992bde7c9ed05ee6029bdf495dd18e6
shawniut <shawniut@gmail.com>
2018-08-01 18:34:50 -0600

    Added mailer

--- app/mailers/appointments_mailer.rb
+++ app/mailers/appointments_mailer.rb
@@ -6,7 +6,7 @@ class AppointmentsMailer < ApplicationMailer
 
     def new_appointment(appointment_id)
       @appointment = Appointment.find(appointment_id)
-      mail(to: 'shawniut@gmail.com',reply_to: 'Travelclinic.calgary@gmail.com', subject: 'A new appointment request received')do |format|
+      mail(to: 'shawniut@gmail.com',reply_to: 'info@travelcliniccalgary.ca', subject: 'A new appointment request received')do |format|
           format.html { render :layout => false }
       end
     end
